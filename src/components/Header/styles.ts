6022383d9d8c68791e720651beae3a1b93187c91
gilles30 <lorison.gilles@gmail.com>
2022-11-19 23:16:57 -0300

    ✔️📌 Feat: Creating header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #d73035;
+  height: 198px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1216px;
+`;
