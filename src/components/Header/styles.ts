df824f7d4ee324cdae1245b6cc1110f4e9eb36c5
gilles30 <lorison.gilles@gmail.com>
2022-11-22 00:04:58 -0300

    😏feat: Created Order Header Board modal component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -10,7 +10,21 @@ export const Container = styled.header`
 
 export const Content = styled.div`
   display: flex;
+  align-items: center;
   justify-content: space-between;
   width: 100%;
   max-width: 1216px;
+  .page-details {
+    h1 {
+      color: #fff;
+      font-size: 2rem;
+    }
+    h2 {
+      color: #fff;
+      opacity: 0.9;
+      font-size: 1rem;
+      font-weight: 400;
+      margin-top: 6px;
+    }
+  }
 `;
---
+++ src/components/OrdersBoard/index.tsx
@@ -0,0 +1,43 @@
+import { Board, BoardHeader, OrderContainer } from './styles';
+import { Order } from '../../types/Order';
+import { OrderModal } from '../OrderModal';
+import { useState } from 'react';
+
+interface OrdersBoardProps {
+  icon: any;
+  title: string;
+  orders: Order[];
+}
+
+export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
+  function handleOpenOrder(order: Order) {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  }
+  return (
+    <Board>
+      <OrderModal order={selectedOrder} visible={isModalVisible} />
+      <BoardHeader>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>({orders.length})</span>
+      </BoardHeader>
+      {orders.length > 0 && (
+        <OrderContainer>
+          {orders.map((order) => (
+            <button
+              type="button"
+              key={order._id}
+              onClick={() => handleOpenOrder(order)}
+            >
+              <strong>Mesa {order.table}</strong>
+              <span>{order.products.length} itens</span>
+            </button>
+          ))}
+        </OrderContainer>
+      )}
+    </Board>
+  );
+}
